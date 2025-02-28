1fc6f80f5b63f47fc4472c53f1891dc8397a40c3
TechFosters <akkihere8@gmail.com>
2025-02-28 10:05:41 +0000

    objects introduction

--- 02basics/object_part1.js
+++ 02basics/object_part1.js
@@ -1,9 +1,9 @@
-const e_id = Symbol("key1")
+const e_id = Symbol("key1") //used for declaring symbol as a key
 const myProfile ={
     userName: "TF",
     "full name": "Tech Fosters", //key with spaces are declared using ""
     age: 35,
-    [e_id]: "TF101",
+    [e_id]: "TF101", //used for declaring symbol as key else e_id will be of type string
     location: "Delhi",
     isLoggedIn: true,
     Working_Days: ["Mon", "Tues", "Sat"],
@@ -19,14 +19,15 @@ console.log(myProfile[e_id], typeof(myProfile[e_id]))
 console.log(myProfile.isLoggedIn, typeof(myProfile.isLoggedIn))
 console.log(myProfile.Working_Days, typeof(myProfile.Working_Days))
 
-//Object.freeze(myProfile);
+//Object.freeze(myProfile); //this prevents object from updating
 myProfile.email = "vishal@gmail.com"
 console.log(myProfile, typeof(myProfile));
 
 myProfile.greeting = function(){
     console.log("Hello JS User");
-    console.log(`Hello JS User ${this.userName}`)
+    console.log(`Hello JS User ${this.userName}`) //this refers to the calling object
     
 }
-console.log(myProfile.greeting)
-console.log(myProfile.greeting())
+console.log(myProfile.greeting) // returns [Function (anonymous)]
+//console.log(myProfile.greeting())
+
