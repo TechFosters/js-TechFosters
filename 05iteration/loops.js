d7b50a9fc000c5d9ffdd3481668ac8c09dcfaeee
TechFosters <akkihere8@gmail.com>
2025-03-01 18:29:20 +0000

    loops

---
+++ 05iteration/loops.js
@@ -0,0 +1,41 @@
+//for
+
+for(let i = 0; i < 10; i++){
+    const element = i;
+    if(element == 5){
+        console.log("High Five");
+        
+    }
+    console.log(element)
+
+} 
+
+arr = [10,20,30,40,50]
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(`element ${element} @ index: ${index}`);
+    
+}
+myArr = [2,3,5,7,11,13,17,19,23,39]
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    if(element == 5){
+        console.log('5 MIL GYA')
+        break;
+        
+    }
+    console.log(element);
+    
+}
+
+myArr = [2,3,5,7,11,13,17,19,23,39]
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    if(element == 5){
+        console.log('5 MIL GYA')
+        break;
+        
+    }
+    console.log(element);
+    
+}
