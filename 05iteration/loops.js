d9750c7e316cff1a9d339e981acf4f0bd08d7798
TechFosters <akkihere8@gmail.com>
2025-03-01 18:49:35 +0000

    for loop, break and continue

--- 05iteration/loops.js
+++ 05iteration/loops.js
@@ -1,5 +1,7 @@
 //for
-
+//1. variable initialization
+//2. limit(loop kaha tak chlega)
+//3. icrement/decrement
 for(let i = 0; i < 10; i++){
     const element = i;
     if(element == 5){
@@ -10,6 +12,7 @@ for(let i = 0; i < 10; i++){
 
 } 
 
+//for loop with array
 arr = [10,20,30,40,50]
 for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
@@ -27,13 +30,25 @@ for (let i = 0; i < myArr.length; i++) {
     console.log(element);
     
 }
-
+//break and continue
 myArr = [2,3,5,7,11,13,17,19,23,39]
 for (let i = 0; i < myArr.length; i++) {
     const element = myArr[i];
-    if(element == 5){
-        console.log('5 MIL GYA')
+    if(element == 13){
+        console.log('13 MIL GYA')
         break;
+        
+    }
+    console.log(element);
+    
+}
+
+myArray = [2,3,5,7,11,13,17,19,23,39]
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    if(element == 13){
+        console.log('13 MIL GYA')
+        continue;
         
     }
     console.log(element);
