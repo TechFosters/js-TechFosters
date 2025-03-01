7ce47609458eeb744746167bf1f0d84869316c55
TechFosters <akkihere8@gmail.com>
2025-03-01 19:22:44 +0000

    while doWhile Intro

---
+++ 05iteration/while_dowhile.js
@@ -0,0 +1,14 @@
+//whlile
+let i = 1
+while(i<10){
+    console.log(`count is ${i}`)
+    i++;
+}
+
+//doWhile
+let score = 30
+do {
+    console.log(`your score is ${score}`);
+    score++
+} while (score < 10)
+
