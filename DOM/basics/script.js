df56b0da4eeb4cd522ba8807880dfce9d78d4b03
TechFosters <akkihere8@gmail.com>
2025-03-05 16:56:39 +0000

    dom basics

---
+++ DOM/basics/script.js
@@ -0,0 +1,21 @@
+const div = document.createElement('div')
+console.log(div);
+
+div.className = "main"
+div.id = 'block'
+
+div.style.backgroundColor='#ffbf00'
+'#ffbf00'
+div.style.padding='12px'
+'12px'
+const addText = document.createTextNode('TechFosters')
+div.appendChild(addText)
+document.body.appendChild(div)
+div.style.borderRadius="50%"
+
+
+div.style.padding="50px"
+'50px'
+
+
+ 
